c790ff0ada1933b2efb57708ffd2cd04c4ae8946
Ian Macharia <ianmachariak17@gmail.com>
2022-12-16 08:16:14 +0300

    [modify] modified the login controller

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# 
 urlpatterns = [
     path('', views.login_view, name="login_view"),
     path('register', views.register_view, name="registration_view"),
